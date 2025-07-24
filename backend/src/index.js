065483d49c1a4a7b2540255d20641d18bbe631a8
lokesh <lokesh@test.com>
2025-07-24 12:12:45 +0530

    docker file added

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
@@ -18,23 +16,33 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://chat-app-frontend.vercel.app",
+  "https://chat-app-frontend-git-main-lokesh-36s-projects.vercel.app",
+  "https://chat-app-frontend-three-liard.vercel.app",
+  "https://chat-app-frontend-8s6saenjz-lokesh-36s-projects.vercel.app"
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
 
+// API routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+// No need to serve frontend — it's deployed separately
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,7 +7,14 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://chat-app-frontend.vercel.app",
+      "https://chat-app-frontend-three-liard.vercel.app",
+      "https://chat-app-frontend-git-main-lokesh-36s-projects.vercel.app",
+      "https://chat-app-frontend-8s6saenjz-lokesh-36s-projects.vercel.app"
+    ],
+    credentials: true,
   },
 });
 
@@ -24,7 +31,6 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
