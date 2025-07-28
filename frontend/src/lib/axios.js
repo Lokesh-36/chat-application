b15600abbd4275fcef5d5f6b16552829184ffe94
lokesh <lokesh@test.com>
2025-07-29 03:10:25 +0530

    remove api from url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://13.60.180.235:5001/api",
+  baseURL: "http://13.60.180.235:5001",
   withCredentials: true,
 });
