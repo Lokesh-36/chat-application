a5fe4f31588dff2955107ae7a40c3d03a7870f4c
lokesh <lokesh@test.com>
2025-07-29 11:04:46 +0530

    api added in axios url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://13.60.180.235:5001",
+  baseURL: "http://13.60.180.235:5001/api",
   withCredentials: true,
 });
